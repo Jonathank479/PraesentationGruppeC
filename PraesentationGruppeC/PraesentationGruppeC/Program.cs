e1c593cda3e850d0a81d1d1ae3f0ed0f015e1a74
Jonathank479 <jonathan.krockhaus@student.jade-hs.de>
2021-03-18 11:03:08 +0100

    Update Program.cs

--- PraesentationGruppeC/PraesentationGruppeC/Program.cs
+++ PraesentationGruppeC/PraesentationGruppeC/Program.cs
@@ -8,9 +8,21 @@ namespace PraesentationGruppeC
 {
     class Program
     {
-        static void Main(string[] args)// hiiii
+        static void Main(string[] args)
         {
-            Console.WriteLine("helloworld");// HALLOuuu
+            Console.WriteLine("Geben Sie Ihren Namen ein");
+
+            string name;
+            name = Console.ReadLine();
+
+            if (name == "Merve")
+                Console.WriteLine("Hallo, Merve");
+            if (name == "Jonathan")
+                Console.WriteLine("Hallo, Jonathan");
+            else
+                Console.WriteLine("Du bist nicht in Gruppe C");
+            
+            Console.ReadKey();
         }
     }
 }
