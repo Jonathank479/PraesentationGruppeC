124f45e6aa15f1234f0dc01f2e3f03b7bf732bfe
Mervenur Mutu <80036734+MervenurMutu@users.noreply.github.com>
2021-03-22 13:17:10 +0100

    Update Program.cs

--- PraesentationGruppeC/PraesentationGruppeC/Program.cs
+++ PraesentationGruppeC/PraesentationGruppeC/Program.cs
@@ -13,7 +13,6 @@ namespace PraesentationGruppeC
             string name;
             name = Console.ReadLine();
 
-<<<<<<< Updated upstream
             {
                 if (name == "Ming")
                 {
@@ -37,17 +36,6 @@ namespace PraesentationGruppeC
                 }
                
             }
-=======
-            if (name == "Merve")
-                Console.WriteLine("Hallo, Merve");
-            if (name == "Jonathan")
-                Console.WriteLine("Hallo, Jonathan");
-            if (name == "Djongoue")
-                Console.WriteLine("Hallo, Djongoue");
-            else
-                Console.WriteLine("Du bist nicht in Gruppe C");
->>>>>>> Stashed changes
-            
 
             Console.ReadKey();
         }
